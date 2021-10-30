25502d0a3dc29bbc71bf2e0dbe293e524299f0fd
Eduardo-Santos-01 <eduardosantos123444@gmail.com>
2021-10-30 12:12:07 -0300

    up

--- interative.js
+++ interative.js
@@ -2,7 +2,7 @@ $(function(){
    button=$('#botão--menu');
    sideBar= $(".sidebar")
     
-   button.on('click',function btnOn(){
+   button.on('click',function(){
        sideBar.toggleClass("ação-menu");
       button.toggleClass('bx-menu bx-menu-alt-right');  
     
