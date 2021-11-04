e05f1b9b8627d227c551363d3641dfc08a8967a3
Eduardo-Santos-01 <eduardosantos123444@gmail.com>
2021-11-04 19:03:28 -0300

    mod

--- interative.js
+++ interative.js
@@ -1,12 +1,15 @@
 $(function(){
    button=$('#botão--menu');
-   sideBar= $(".sidebar")
+   sideBar= $(".sidebar");
+   action=$('.ação-menu')
+   login=$('.blog')
     
    button.on('click',function(){
        sideBar.toggleClass("ação-menu");
-      button.toggleClass('bx-menu bx-menu-alt-right');  
-    
-  })
+      button.toggleClass('bx-menu bx-menu-alt-right');
+      login.toggleClass(' bx-log-out bx-log-in')
+  });
+
 })
 
 
