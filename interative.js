c254600073f783f62b7b0052912700b87c847074
Eduardo-Santos-01 <eduardosantos123444@gmail.com>
2021-11-28 22:05:12 -0300

    add nova função

--- interative.js
+++ interative.js
@@ -1,16 +1,17 @@
 $(function(){
-   button=$('#botão--menu');
-   sideBar= $(".sidebar");
-   action=$('.ação-menu')
-   login=$('.blog')
-    
-   button.on('click',function(){
-       sideBar.toggleClass("ação-menu");
-      button.toggleClass('bx-menu bx-menu-alt-right');
-      login.toggleClass(' bx-log-out bx-log-in')
-  });
+button=$('#botão--menu');
+sideBar= $(".sidebar");
+action=$('.ação-menu');
+login=$('.blog');
 
-})
+    
+ 
+   button.on('click',function(){
+      sideBar.toggleClass("ação-menu");
+      button.toggleClass('bx-menu bx-menu-alt-right');
+      login.toggleClass(' bx-log-out bx-log-in');
+      })
+  })
 
 
 
