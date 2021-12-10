5f4a187163a677509464b37956aa1a9790514d25
EduardoSantos-2020 <eduardosantos123444@gmail.com>
2021-12-09 21:52:23 -0300

    search

--- interative.js
+++ interative.js
@@ -2,14 +2,11 @@ $(function(){
 button=$('#botão--menu');
 sideBar= $(".sidebar");
 action=$('.ação-menu');
-login=$('.blog');
 
-    
- 
    button.on('click',function(){
       sideBar.toggleClass("ação-menu");
-      button.toggleClass('bx-menu bx-menu-alt-right');
-      login.toggleClass(' bx-log-out bx-log-in');
+      button.toggleClass('bx-menu bx-x');
+      
       })
   })
 
